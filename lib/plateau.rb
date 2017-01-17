717b88b61bdbd2a004082a08a8e0d2a395677d05
Hanif Fakira <fakira.work@gmail.com>
2017-01-17 14:54:05 +0000

    US1 UT exception if size < 1

--- lib/plateau.rb
+++ lib/plateau.rb
@@ -2,7 +2,14 @@ class Plateau
   attr_reader :max_x, :max_y
 
   def initialize(max_x, max_y)
-    @max_x = max_x
-    @max_y = max_y
+    @max_x = check_size(max_x) || max_x
+    @max_y = check_size(max_y) || max_y
   end
+
+  private
+    def check_size coordinate
+      if coordinate < 1
+        raise "Coordinate size is non-positive: Choose positive coordinates"
+      end
+    end
 end
--- spec/plateau_spec.rb
+++ spec/plateau_spec.rb
@@ -1,15 +1,25 @@
 describe Plateau do
   subject(:plateau) { described_class.new(5, 9)}
-  
-  it 'create a plateau of size x' do
-    plateau = Plateau.new(5, 9)
 
+  it 'create a plateau of size x' do
     expect(plateau.max_x).to eq 5
   end
 
   it 'create a plateau of size y' do
-    plateau = Plateau.new(5, 9)
-
     expect(plateau.max_y).to eq 9
+  end
+
+  describe 'raise exception if coordinates are out of bounds' do
+    it 'x out of bounds' do
+      expect { Plateau.new(-1, 5) }.to raise_error "Coordinate size is non-positive: Choose positive coordinates"
+    end
+
+    it 'y out of bounds' do
+      expect { Plateau.new(1, 0) }.to raise_error "Coordinate size is non-positive: Choose positive coordinates"
+    end
+
+    it 'x and y out of bounds' do
+      expect { Plateau.new(0, -3) }.to raise_error "Coordinate size is non-positive: Choose positive coordinates"
+    end
   end
 end
