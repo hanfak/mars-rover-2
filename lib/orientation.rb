89c654c2694d5c2d21644df435ef86fc70c9dff7
Hanif Fakira <fakira.work@gmail.com>
2017-01-18 16:09:58 +0000

    US2 orientation can only have four directions UT

--- lib/orientation.rb
+++ lib/orientation.rb
@@ -2,6 +2,13 @@ class Orientation
   attr_reader :direction
 
   def initialize(direction)
-    @direction = direction
+    @direction = check_direction(direction) || direction
   end
+
+  private
+    def check_direction(direction)
+      unless ['N','S','E','W'].include? direction
+        raise "Not a direction"
+      end
+    end
 end
--- spec/orientation_spec.rb
+++ spec/orientation_spec.rb
@@ -1,12 +1,31 @@
 describe Orientation do
   describe "instantiation" do
-    it 'has an orientation' do
+    it 'can have a West direction ' do
       orientation = Orientation.new('W')
 
       expect(orientation.direction).to eq 'W'
     end
 
-    xit 'can only have four directions'
-    xit 'raises error if direction is not a compass point'
+    it 'can have a North direction' do
+      orientation = Orientation.new('N')
+
+      expect(orientation.direction).to eq 'N'
+    end
+
+    it 'can have an East direction' do
+      orientation = Orientation.new('E')
+
+      expect(orientation.direction).to eq 'E'
+    end
+
+    it 'can have a South direction' do
+      orientation = Orientation.new('S')
+
+      expect(orientation.direction).to eq 'S'
+    end
+
+    it 'raises error if direction is not a compass point' do
+        expect { Orientation.new('K') }.to  raise_error "Not a direction"
+    end
   end
 end
